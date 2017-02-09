27f842eb5c0d26b44104b03a1b9b947ec586843a
Jiwon Kang and Lawrence Eby <jiwon+lawrence@>
2017-02-09 13:35:17 -0800

    Add extra search functionality

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -20,6 +20,18 @@ namespace CdOrganizer
         Cd newCd = Cd.GetCd(parameters.id);
         return View["cd.cshtml", newCd];
       };
+
+      Get["/artist-list"] = _ => {
+        Cd.SetArtistList();
+        return View["artist-list.cshtml", Cd.GetArtistList()];
+      };
+
+      Get["/artist-list/{artistName}"] = parameters =>
+      {
+        string artistNameInput = parameters.artistName;
+        Cd.SetAlbumList(artistNameInput);
+        return View["single-artist.cshtml", Cd.GetAlbumList()];
+      };
     }
   }
 }
--- Objects/Cd.cs
+++ Objects/Cd.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace CdOrganizer.Objects
 {
@@ -8,6 +9,8 @@ namespace CdOrganizer.Objects
     private string _album;
     private int _id;
     private static List<Cd> _instances = new List<Cd>{};
+    private static List<Cd> _albumCheck = new List<Cd>{};
+    private static List<Cd> _artistCheck = new List<Cd>{};
 
     public Cd(string artist, string album)
     {
@@ -46,9 +49,48 @@ namespace CdOrganizer.Objects
       return _id;
     }
 
+    public static void SetArtistList()
+    {
+      int count = 1;
+      foreach(Cd cd in _instances)
+      {
+        _artistCheck.Add(cd);
+        for(int index = count; index < _artistCheck.Count; index++)
+        {
+          if(cd.GetArtist() == _artistCheck[index].GetArtist())
+          {
+            _artistCheck.RemoveAt(index);
+          }
+        }
+        count++;
+      }
+    }
+
+    public static List<Cd> GetArtistList()
+    {
+      return _artistCheck;
+    }
+
     public static Cd GetCd(int cdId)
     {
       return _instances[cdId - 1];
+    }
+
+    public static void SetAlbumList(string artistName)
+    {
+      _albumCheck.Clear();
+      foreach(Cd cd in GetAll())
+      {
+        if(cd.GetArtist() == artistName)
+        {
+          _albumCheck.Add(cd);
+        }
+      }
+    }
+
+    public static List<Cd> GetAlbumList()
+    {
+      return _albumCheck;
     }
   }
 }
